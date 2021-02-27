161e5c99590e3becfc3f06347e83412e868069cd
Rohil Patel <patelrohil2006@icloud.com>
2021-02-27 05:14:40 +0000

    Finished sockets

--- static/script/script.js
+++ static/script/script.js
@@ -1,5 +1,23 @@
 var es = new EventSource("/sse");
 es.onmessage = (d)=>{
+  if(JSON.parse(d.data).type == "connection"){
+    s(JSON.parse(d.data).YourID);
+  }
   var xhr = new XMLHttpRequest();
-  xhr.open("")
+  xhr.open("POST","/sse/log");
+  xhr.send(d.data);
 };
+function s(id){
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST","/sse?id=" + id);
+  xhr.send(JSON.stringify({
+    opcode: "0000000",
+    data: "yo"
+  }));
+}
+
+const username = document.getElementById('username-input');
+	
+username.addEventListener('keydown', e => {
+	document.getElemenyById('solo').innerHTML = username.value
+});
--- utilities/sse.js
+++ utilities/sse.js
@@ -1,19 +1,31 @@
 var rout = require("express").Router();
 var client = require("srnet-client");
+rout.use(require("express").text());
+var clis = {};
 rout.get("/",(req,res)=>{
   res.writeHead(200,{"Content-Type":"text/event-stream"});
   var cli = new client("https://the-mappable-universe.classcoders.repl.co/server");
   cli.on("notAsk",d=>{
     delete d.client;
-    res.write("data:" + JSON.stringify(d) + "\r\n\r\n");
+    var a = {
+      type: "data",
+      data: d
+    };
+    if(res.socket.readyState == "open") res.write("data:" + JSON.stringify(a) + "\r\n\r\n");
   });
   cli.on("connect",id=>{
-    cli.send("0000000",id);
+    clis[id] = cli;
+    if(res.socket.readyState == "open") res.write("data:" + JSON.stringify({type:"connection",YourID:id}) + "\r\n\r\n");
   });
   cli.connect();
 });
-rout.use("/log",require("express").text());
+rout.post("/",(req,res)=>{
+  if(clis[req.query.id]._cli.socket.readyState == "open"){
+    var json = JSON.parse(req.body);
+    clis[req.query.id].send(json.opcode,json.data);
+  }
+});
 rout.post("/log",(req,res)=>{
-  console.log(req.body);
+  console.log("\x1b[1m\x1b[33m[LOG]\x1b[m \x1b[1m" + req.body + "\x1b[m");
 });
 module.exports = rout;
